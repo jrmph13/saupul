e96e7a21e9cacf2846305f807929544389967228
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-08 23:34:35 +0000

    chore(ui): implement auth tokens

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_308b09948a6e4aa1b4ebfe91f876ab24 = () => {
-  return '2025-03-08T03:32:56Z';
+const node_2bf7554a55f14a72868607be03cbd051 = () => {
+  return '2025-03-08T23:34:35Z';
 };
