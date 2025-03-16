988f75b82228fe325c7572507701573c4752a0f0
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-16 18:42:31 +0000

    feat(utils): decouple auth tokens

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c2449083d0f452e8a20adfd425c2271 = () => {
-  return '2025-02-05T10:29:38Z';
+const node_ce296d5a0b604d22a1588555e77a7c34 = () => {
+  return '2025-03-16T18:42:31Z';
 };
