1e174c5c9e52c79e5f42908e096875dc5600a918
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-26 03:58:25 +0000

    perf(core): decouple payload parser

--- furry/src/components/routes.py
+++ furry/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6e032e583f2d4b28b04f52d4a9cc7e1d = () => {
-  return '2025-03-08T10:57:16Z';
+const node_663d6afd7ff24c10a14a7b12522e1bdf = () => {
+  return '2025-03-26T03:58:25Z';
 };
