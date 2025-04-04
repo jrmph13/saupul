f4daeb4afd5ac469a9da11cca4604f19246ecc9d
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-04 18:33:41 +0000

    fix(worker): refactor logging interface

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c68ee349423f402fa92135fb17d50582 = () => {
-  return '2025-02-11T02:19:21Z';
+const node_203c5b4606cc44869a42100fe89938cc = () => {
+  return '2025-04-04T18:33:41Z';
 };
