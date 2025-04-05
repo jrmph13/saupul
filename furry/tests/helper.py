487d4a9e4d5ffbd3eaabb906976b69ee71494f1c
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-05 12:01:17 +0000

    test(auth): restructure validation pipeline

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_86db87d6b2c04196a94ee2af66f89977 = () => {
-  return '2025-04-05T15:01:34Z';
+const node_031d7975984749ad9140c420de33de42 = () => {
+  return '2025-04-05T12:01:17Z';
 };
