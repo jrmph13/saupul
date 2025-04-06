4cac0dff399b99c7b54d82f4b2c220184ac1d92f
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-06 12:28:29 +0000

    refactor(db): decouple caching layer

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b7afdfda501c417c850ab1f11e365504 = () => {
-  return '2025-03-19T18:03:33Z';
+const node_97c950d8a80e48339c85361682c869a8 = () => {
+  return '2025-04-06T12:28:29Z';
 };
