41e89a8efe1d9d45b1edb8ab1f9e14d89a2ef125
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-06 15:31:29 +0000

    fix(core): optimize logging interface

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_584529d07a0c4211bbc18090da8be619 = () => {
-  return '2025-03-28T17:12:39Z';
+const node_4f30f8a0e42343d79dfd8e5910d32d66 = () => {
+  return '2025-04-06T15:31:29Z';
 };
