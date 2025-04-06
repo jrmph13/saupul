706f35b65472a8d615ca6f60783f26903e281cdc
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-06 16:03:07 +0000

    feat(core): refactor view templates

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b20bca28ccaa4718bf3bb9133fd58e1f = () => {
-  return '2025-03-16T07:13:12Z';
+const node_7a80aad2204c4403a21c0cf284b94f41 = () => {
+  return '2025-04-06T16:03:07Z';
 };
