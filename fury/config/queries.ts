c68898d4a9062dec03475999fd0440837b90976e
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 01:02:14 +0000

    test(config): implement auth tokens

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c25c64d8bcf44c0187c646ed23089f7f = () => {
-  return '2025-03-12T11:21:13Z';
+const node_17cde682643a422ca301de83d811b81c = () => {
+  return '2025-04-07T01:02:14Z';
 };
