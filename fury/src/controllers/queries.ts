c2aad29a4ea54a2d130e32e583257d225140014a
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-08 14:31:37 +0000

    perf(config): integrate auth tokens

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b2b64e6606ff4d1e940b8dc7fc399b5d = () => {
-  return '2025-04-06T15:48:48Z';
+const node_d25bfc2a6aeb443dabebeb5c87e13c28 = () => {
+  return '2025-04-08T14:31:37Z';
 };
