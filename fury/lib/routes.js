5632ff0ff3b6aeb22822fe7fd47c83386fd26e65
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-08 15:10:01 +0000

    fix(auth): fix view templates

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_760824e5c6604b8186c2ea4af3e8f399 = () => {
-  return '2025-04-05T13:50:41Z';
+const node_fee7457dc0f04acfa6c071b1f21d4c42 = () => {
+  return '2025-04-08T15:10:01Z';
 };
