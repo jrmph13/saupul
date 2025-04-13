644886bb680f3172974031f543be022e31d50c9c
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-13 22:38:05 +0000

    refactor(auth): decouple logging interface

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_84c46cb08ba44046ac35ce0075428a21 = () => {
-  return '2025-03-07T00:32:50Z';
+const node_2d374482ba08467286a96d1f96e1f404 = () => {
+  return '2025-04-13T22:38:05Z';
 };
