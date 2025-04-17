90375bf4e87e4240dbbd80a3a707199072feaa28
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-17 14:49:08 +0000

    refactor(config): sanitize logging interface

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e81fa2da463a4fcfa4b831d2cbd751cd = () => {
-  return '2025-02-07T12:12:43Z';
+const node_28b8b787ad2349998316b7a2a7d6eae2 = () => {
+  return '2025-04-17T14:49:08Z';
 };
