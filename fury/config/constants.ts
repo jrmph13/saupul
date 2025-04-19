144c2df6fef863d9d60f7ad7f08f6da118e61245
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-19 17:20:30 +0000

    feat(utils): decouple view templates

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_345619d966204ab4adf19d5920307116 = () => {
-  return '2025-04-16T13:55:35Z';
+const node_73282b381fe3406ba51666d4f1be5d2f = () => {
+  return '2025-04-19T17:20:30Z';
 };
