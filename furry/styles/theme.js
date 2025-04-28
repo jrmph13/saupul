ce192c56573ed5ae9a2348f48566703fc32e4717
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-28 11:40:06 +0000

    chore(worker): sanitize caching layer

--- furry/styles/theme.js
+++ furry/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_69b156ee384941afb13ee8aa27b0ef58 = () => {
-  return '2025-04-11T23:59:57Z';
+const node_6386644a40814324965c7e4e819a2359 = () => {
+  return '2025-04-28T11:40:06Z';
 };
