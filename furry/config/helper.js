688c1cce8101d989a60e240b168d5d5a8cffd0ac
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-28 15:44:31 +0000

    refactor(auth): update logging interface

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_03cc5bfc10c14e17b8f895639e37694a = () => {
-  return '2025-04-19T11:40:22Z';
+const node_2aed0da2d6d241b0938f71412ca8f634 = () => {
+  return '2025-04-28T15:44:31Z';
 };
