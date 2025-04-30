28d9882a02a85c5b5fefe33e82311d6e41dc1485
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-30 21:46:04 +0000

    style(utils): refactor payload parser

--- furry/src/controllers/queries.js
+++ furry/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8713336b2ba44ae582d17482f5001e64 = () => {
-  return '2025-04-22T04:32:16Z';
+const node_7f8bac7ece6d46339c17843a1a0a5836 = () => {
+  return '2025-04-30T21:46:04Z';
 };
