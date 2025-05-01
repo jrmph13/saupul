e26889e3a06bbef2698ec690cecc1c23797b19a9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-01 05:47:26 +0000

    fix(db): update auth tokens

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d8a49c0d6d1b42958a4ff28f3b4511e4 = () => {
-  return '2025-04-26T03:04:51Z';
+const node_d8a63568496a405891415662a8485d7e = () => {
+  return '2025-05-01T05:47:26Z';
 };
