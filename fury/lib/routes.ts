f7ba8b34028d7d8ac8ab792bfd6cfe7c415338d9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-03 15:56:42 +0000

    docs(core): integrate auth tokens

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_969afbd356254840ba910968e720e658 = () => {
-  return '2025-04-18T14:57:16Z';
+const node_b0c4c4698b4e456a813d74a150a2e44a = () => {
+  return '2025-05-03T15:56:42Z';
 };
