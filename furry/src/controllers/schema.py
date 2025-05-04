6e08be5335d838b960025da45a17f7b6056e161e
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-04 18:02:09 +0000

    feat(auth): restructure payload parser

--- furry/src/controllers/schema.py
+++ furry/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e2ccd182df1742c7b787bec8fff10d50 = () => {
-  return '2025-03-29T01:18:39Z';
+const node_53b5c4ba697248b2bbb35be02163ee22 = () => {
+  return '2025-05-04T18:02:09Z';
 };
