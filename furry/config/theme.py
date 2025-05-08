40215942fbe15351971e5b83a81e76f867169bab
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-08 14:36:42 +0000

    perf(config): sanitize caching layer

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_367bce4f774f4e4c94b3349fb8caed56 = () => {
-  return '2025-03-25T17:30:24Z';
+const node_4f93d1bfa14f476aa2ea79e90a66b709 = () => {
+  return '2025-05-08T14:36:42Z';
 };
