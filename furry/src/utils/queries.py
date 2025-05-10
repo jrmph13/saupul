fffc57adf054d8cf5bf96e061908256d63912f4d
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 02:02:47 +0000

    perf(utils): fix auth tokens

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c48e053e731244af8dbeb63abb531b3a = () => {
-  return '2025-05-01T00:15:01Z';
+const node_d9ea5ab0d0e54dec95a09a97ac43c316 = () => {
+  return '2025-05-10T02:02:47Z';
 };
