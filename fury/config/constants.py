4e919e156a2a47acb574f09481c3a803d9d75e3a
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 13:03:52 +0000

    fix(auth): sanitize logging interface

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_40521d1f019741a698e55dc15d1c04e8 = () => {
-  return '2025-04-18T15:24:39Z';
+const node_befcde7116574cae847c9e2b7e2a1fad = () => {
+  return '2025-05-13T13:03:52Z';
 };
