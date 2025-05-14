8edc59245441f09cf289c787b54e75f98d6350b4
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-14 18:38:07 +0000

    docs(auth): update middleware routes

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_50eed28c718c4d0fa60cca8d62e0eafb = () => {
-  return '2025-03-13T06:00:05Z';
+const node_8cf03cf1096740059c98b9080ae9db90 = () => {
+  return '2025-05-14T18:38:07Z';
 };
