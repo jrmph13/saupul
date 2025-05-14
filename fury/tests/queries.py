75a01ec58c65d7c65c31136ea6b5682c7d21c734
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-14 12:58:35 +0000

    style(api): decouple validation pipeline

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_499b8af2679845668c2acdf746bce66e = () => {
-  return '2025-05-11T20:25:38Z';
+const node_3ae8de5701714eb5b10cc25211103266 = () => {
+  return '2025-05-14T12:58:35Z';
 };
