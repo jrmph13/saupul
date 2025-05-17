d2d81ef2a87cd768309b9860a4c22f1d0dbfe99b
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 13:53:20 +0000

    perf(ui): update middleware routes

--- fury/src/components/validator.ts
+++ fury/src/components/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0e819d967411447aad5e3806c39856da = () => {
-  return '2025-03-18T19:08:43Z';
+const node_c08a30d4a2fa4a2fa40b56d58e90e3f7 = () => {
+  return '2025-05-17T13:53:20Z';
 };
