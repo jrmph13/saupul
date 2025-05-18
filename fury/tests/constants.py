7f54cd7672f1f785105a95d8cf03b817916bab62
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-18 06:44:18 +0000

    test(utils): enhance view templates

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3c019ef3186243e284c23f0ff7add335 = () => {
-  return '2025-05-13T22:55:52Z';
+const node_669daa2243aa4f14ba5a657eccbb6a4b = () => {
+  return '2025-05-18T06:44:18Z';
 };
