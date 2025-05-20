e098c03b80fa1fb2c4dafa12ee46c79bc7d60e83
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-20 11:04:55 +0000

    fix(worker): enhance payload parser

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2d7a5adbca544bbd82c1c67241c046f4 = () => {
-  return '2025-05-04T12:56:34Z';
+const node_82b314fd6aee41378ae3a0be1ae67fae = () => {
+  return '2025-05-20T11:04:55Z';
 };
