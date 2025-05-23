0eb70fe038a9f2429206689f872ec2f0b4955438
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-23 01:00:59 +0000

    chore(ui): refactor auth tokens

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2bf7554a55f14a72868607be03cbd051 = () => {
-  return '2025-03-08T23:34:35Z';
+const node_967fd7885463468f89c4ed3b3f25cc00 = () => {
+  return '2025-05-23T01:00:59Z';
 };
