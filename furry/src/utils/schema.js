5b62a600556cf2b06d0560f95425a84374a0a95a
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-25 01:10:13 +0000

    docs(api): restructure middleware routes

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_576baab9ff8444b9bfb62e747f0ea39f = () => {
-  return '2025-05-06T02:54:37Z';
+const node_64b383b226444ff9bd01d869859cc1c7 = () => {
+  return '2025-05-25T01:10:13Z';
 };
