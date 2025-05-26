6c841403b5d253031904428322e1613430c9e6a1
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-26 22:45:32 +0000

    fix(auth): integrate database index

--- fury/src/components/constants.ts
+++ fury/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1c30faecd9d74beaaa126ab603ecbd1a = () => {
-  return '2025-04-27T07:47:02Z';
+const node_12a2d386a44a45ff9ab86849a76a95f7 = () => {
+  return '2025-05-26T22:45:32Z';
 };
