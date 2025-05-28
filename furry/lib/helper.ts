0f0a2c24b2113b5c1b40f8f6b78c9a52bca5cade
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-28 08:25:15 +0000

    perf(auth): integrate auth tokens

--- furry/lib/helper.ts
+++ furry/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_72e5cc6de22b42e18a291e1a68446d2e = () => {
-  return '2025-03-08T02:29:32Z';
+const node_521ba5e361ca4d4fb475664b3223524e = () => {
+  return '2025-05-28T08:25:15Z';
 };
