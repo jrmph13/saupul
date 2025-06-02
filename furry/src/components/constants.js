eb5f62925c30b17dc0ffd62adea4e25dfa459048
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-02 17:55:50 +0000

    style(worker): fix middleware routes

--- furry/src/components/constants.js
+++ furry/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2d426f40e1a94e0b95603a601d98b35c = () => {
-  return '2025-05-25T00:14:05Z';
+const node_9eddaaf1eb4c435799d53ac283a917cd = () => {
+  return '2025-06-02T17:55:50Z';
 };
