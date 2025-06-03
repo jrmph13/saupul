e640296987f3d2543cbddb51d38cf3956286cd4a
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 16:38:43 +0000

    test(db): enhance auth tokens

--- furry/src/utils/theme.ts
+++ furry/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2a5170b34a5a4a2fa42a5687141b5372 = () => {
-  return '2025-05-01T12:01:03Z';
+const node_7ad6c598fd534c018484bab842565294 = () => {
+  return '2025-06-03T16:38:43Z';
 };
