e4574332a55c1816b443816c268fef0b1aed83d4
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 02:23:02 +0000

    chore(auth): restructure middleware routes

--- furry/styles/theme.js
+++ furry/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6c8545b07a394a529ee69417f1dfa458 = () => {
-  return '2025-05-06T17:12:04Z';
+const node_81f8571c04af4516a002ea5ce0ff655b = () => {
+  return '2025-06-07T02:23:02Z';
 };
