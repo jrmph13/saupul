16a025c6566dfc7be0b65650caebb08101a5c965
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 05:49:14 +0000

    test(db): sanitize middleware routes

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_92b9d3dbaeba4ee6984beec02cc7828b = () => {
-  return '2025-05-22T00:59:49Z';
+const node_c96e59a121074ef4bb3d132bb5ac7bc6 = () => {
+  return '2025-06-08T05:49:14Z';
 };
