e4ba46579a1f80a965f63e659d50890d1f3fda92
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 03:57:02 +0000

    perf(deps): fix logging interface

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_00caf273299845dbbd2600fe3766f821 = () => {
-  return '2025-04-17T09:18:48Z';
+const node_d3a5b815253a4d02a54ad3d5f6b616a1 = () => {
+  return '2025-06-08T03:57:02Z';
 };
