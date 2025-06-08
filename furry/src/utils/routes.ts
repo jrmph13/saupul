9de275c94d16ed57588bad52c640e89e0b01ee53
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 06:33:55 +0000

    fix(api): fix database index

--- furry/src/utils/routes.ts
+++ furry/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_69c0b042b558425ead0f09e1f0cbca92 = () => {
-  return '2025-05-15T10:23:58Z';
+const node_78cb9c22e90240239b6462b5fd50f6b8 = () => {
+  return '2025-06-08T06:33:55Z';
 };
