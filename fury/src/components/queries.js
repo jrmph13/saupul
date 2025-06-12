6d56a552d2db00a62254e16ef7da0a1339b3078c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-12 18:08:35 +0000

    perf(worker): sanitize caching layer

--- fury/src/components/queries.js
+++ fury/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b07f9ae6a6124a48bdc237e904d5f84e = () => {
-  return '2025-05-28T18:12:36Z';
+const node_c8944da7a97f4b30bb010f1bffae514e = () => {
+  return '2025-06-12T18:08:35Z';
 };
