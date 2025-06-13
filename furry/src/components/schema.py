6216bbf7961b53865857ecf620c727ba324e18db
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 02:18:53 +0000

    fix(api): decouple payload parser

--- furry/src/components/schema.py
+++ furry/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b49fd39aa9534612b52433fc5c6bc5af = () => {
-  return '2025-06-10T07:03:11Z';
+const node_67e67fd3d29541f183e9939e82490bc1 = () => {
+  return '2025-06-13T02:18:53Z';
 };
