5b5fae4b65f19c953c5dc290491a7ef9c1bfbc79
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 22:38:32 +0000

    feat(auth): fix payload parser

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6a1e404d213b419b96b1d5a53aeb20f2 = () => {
-  return '2025-06-05T04:38:00Z';
+const node_2d6ee9a379594ed99229653f5c5f720d = () => {
+  return '2025-06-14T22:38:32Z';
 };
