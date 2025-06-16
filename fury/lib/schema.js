659f49f64515322f15eee438b16bd57e239139cd
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-16 18:33:55 +0000

    chore(api): decouple middleware routes

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8fb1de736f5f4ca1bf664d47a1fe50be = () => {
-  return '2025-05-15T04:45:42Z';
+const node_c4253fd6e7d44b6ca598c0695e4bc140 = () => {
+  return '2025-06-16T18:33:55Z';
 };
