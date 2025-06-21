c172d8744f4030bcb0cbfd414c7145ce9879e868
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 03:41:07 +0000

    fix(utils): enhance middleware routes

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_82b314fd6aee41378ae3a0be1ae67fae = () => {
-  return '2025-05-20T11:04:55Z';
+const node_2394e37eefa24c1ea49896e904e1e69d = () => {
+  return '2025-06-21T03:41:07Z';
 };
