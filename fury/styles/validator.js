3780e0e77077482eb373b4f5fc19aa9f08e974e5
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 08:11:35 +0000

    chore(core): refactor payload parser

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c776909f67b4816b32ca5cc0c84476b = () => {
-  return '2025-05-20T14:57:33Z';
+const node_ab708199feb942098ba6dbd01cda5d17 = () => {
+  return '2025-06-21T08:11:35Z';
 };
