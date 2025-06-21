76cb15893dc59664e834d3d04be3a3ec9a617284
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 19:54:30 +0000

    docs(db): fix auth tokens

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_69c9106f13a44195ac8b1d1b28e64bcf = () => {
-  return '2025-04-22T14:51:47Z';
+const node_7c5d24d66520463a9663bff70efb8735 = () => {
+  return '2025-06-21T19:54:30Z';
 };
