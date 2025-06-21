3698c4971fe41e81d288cb7ee04c0e8146d1c87f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 11:30:48 +0000

    style(api): sanitize caching layer

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_726c88f66c8942b380e3155a18e07255 = () => {
-  return '2025-06-14T02:37:52Z';
+const node_d539dc87ed0c4430ba2f07cf22f69d57 = () => {
+  return '2025-06-21T11:30:48Z';
 };
