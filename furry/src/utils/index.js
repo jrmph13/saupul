f4547c98b711035c9c58ae2c3d29f20bf0795f4c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-23 09:12:57 +0000

    docs(config): optimize middleware routes

--- furry/src/utils/index.js
+++ furry/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_36d894b2957b476095453764d262d74c = () => {
-  return '2025-06-09T20:08:23Z';
+const node_441f4e97708d45d2b60768fcb09df4e5 = () => {
+  return '2025-06-23T09:12:57Z';
 };
