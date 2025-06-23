b09a1967c7da961d968261ad9294d893ea131e79
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-23 05:32:26 +0000

    feat(deps): fix database index

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7dfcd8bc00b64f9d83f62e35b2552bd4 = () => {
-  return '2025-05-14T04:13:57Z';
+const node_8012482eba6146bd9f79ae7ec7845e30 = () => {
+  return '2025-06-23T05:32:26Z';
 };
