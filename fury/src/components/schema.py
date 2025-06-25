65cda0db386e69f5f16e9f7d031a07d5924f98b3
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-25 01:45:03 +0000

    perf(core): sanitize auth tokens

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9b10eb1962834c96bffc40d2c154a742 = () => {
-  return '2025-04-24T23:24:36Z';
+const node_0918eff6963d4ee0994fc157b92a9a4f = () => {
+  return '2025-06-25T01:45:03Z';
 };
