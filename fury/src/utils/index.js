02f5ba38dff94deb03004ebc06eb6f062c0d0f29
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-26 12:13:33 +0000

    perf(worker): fix logging interface

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b36e0ae58687437e908c8f0ecf870129 = () => {
-  return '2025-06-06T09:29:39Z';
+const node_86e298b81c8f41dea1a8b7dbbf523b6d = () => {
+  return '2025-06-26T12:13:33Z';
 };
