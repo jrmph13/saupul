67365a2e56ef5aa991fbc743cf7f41242a7fa049
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 20:13:29 +0000

    refactor(deps): sanitize database index

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_18272434af7c474695a45d94d152a96b = () => {
-  return '2025-04-05T04:13:23Z';
+const node_92d683d6394f438389ddda43920c2757 = () => {
+  return '2025-06-29T20:13:29Z';
 };
