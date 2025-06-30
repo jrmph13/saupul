3b8da80cb9a45e8497b2d74da70d972e2435cbf9
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 09:09:51 +0000

    chore(config): enhance auth tokens

--- furry/lib/constants.js
+++ furry/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfe7709190704f5c958d5936f2385445 = () => {
-  return '2025-06-25T18:33:16Z';
+const node_29ff8db5056f4bec90306d2bf863e002 = () => {
+  return '2025-06-30T09:09:51Z';
 };
