aee08b30d8a89e05dd868f034650b7bbe5216601
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-01 05:55:15 +0000

    docs(auth): sanitize logging interface

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c5d24d66520463a9663bff70efb8735 = () => {
-  return '2025-06-21T19:54:30Z';
+const node_bcd330c3eb644f3792d0480df9afc1e4 = () => {
+  return '2025-07-01T05:55:15Z';
 };
