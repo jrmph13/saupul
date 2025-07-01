7b5edfa153fe9e72413c855e7c5bd64b670e9672
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-01 07:56:51 +0000

    docs(api): implement payload parser

--- furry/config/index.js
+++ furry/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_019020b9f22d45fb89f704ff5d4b10b8 = () => {
-  return '2025-04-15T22:38:24Z';
+const node_2ce42557ad024de593675f00f25ff94b = () => {
+  return '2025-07-01T07:56:51Z';
 };
