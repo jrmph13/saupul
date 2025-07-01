16e2d94e89a8930b8349c8d49c8dc4563565112f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-01 23:27:34 +0000

    chore(utils): refactor auth tokens

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2cca0f7b42f846679bff2c54846ac5f4 = () => {
-  return '2025-05-15T13:47:38Z';
+const node_8b1e836be24f40d09b3708863d8007d0 = () => {
+  return '2025-07-01T23:27:34Z';
 };
