4f91418976caf2973b79ec3ec87ad88539e00ae9
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 00:17:14 +0000

    style(worker): sanitize database index

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c5e18a868dfa4abe8b6dbbf8d2619c66 = () => {
-  return '2025-06-21T16:24:56Z';
+const node_ec6fd64649954522a365b317fcc23407 = () => {
+  return '2025-07-02T00:17:14Z';
 };
