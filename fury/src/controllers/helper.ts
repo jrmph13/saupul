4d24779e75c8c1719bce0af3ae8fdd3235adce33
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 14:26:16 +0000

    chore(ui): refactor session store

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8012482eba6146bd9f79ae7ec7845e30 = () => {
-  return '2025-06-23T05:32:26Z';
+const node_2605237816a541fab1bed1559eb25c75 = () => {
+  return '2025-07-02T14:26:16Z';
 };
