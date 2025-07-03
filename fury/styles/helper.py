fefbfef6d06d093f7a995690f41df9401a51eca9
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-03 13:14:09 +0000

    fix(ui): enhance session store

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70bab65534724adf87f8a1dae23807d5 = () => {
-  return '2025-03-30T06:50:43Z';
+const node_ae049eda31f24ed59aaa919238baae94 = () => {
+  return '2025-07-03T13:14:09Z';
 };
