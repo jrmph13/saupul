19174be88d8580e1b8f7d94f3b36df1639d40a63
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-04 23:02:46 +0000

    docs(auth): restructure caching layer

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_375aecbd281f47fea31870b01408363e = () => {
-  return '2025-04-10T21:45:39Z';
+const node_1e0af0c576dd48f7ab78d0eb1bce6a4e = () => {
+  return '2025-07-04T23:02:46Z';
 };
