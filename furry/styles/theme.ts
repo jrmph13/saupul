3f06fee1da0b834dd49a4842837d5b2f3e5be960
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 17:24:43 +0000

    refactor(auth): optimize database index

--- furry/styles/theme.ts
+++ furry/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b4ef65b0d1054e44ab2a4c4fd9c9b71a = () => {
-  return '2025-04-07T05:26:38Z';
+const node_3495a31cc65341b5add6705cabd0a184 = () => {
+  return '2025-07-05T17:24:43Z';
 };
