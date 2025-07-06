3110c0036a10abc10997fdcaf3fb9cef608f8671
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-06 21:07:08 +0000

    fix(utils): integrate database index

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d268022394ed4b169f245f5e62bf740d = () => {
-  return '2025-06-09T05:44:45Z';
+const node_9d23fcb27cb84e17935278b75163f636 = () => {
+  return '2025-07-06T21:07:08Z';
 };
