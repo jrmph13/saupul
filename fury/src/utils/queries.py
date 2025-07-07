f01c221370fe2ec5f778a86a31da4a24e74324a4
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 15:29:47 +0000

    test(auth): optimize payload parser

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_84b08c6a46ec4533a0e9e1dfb8cdf407 = () => {
-  return '2025-07-04T12:52:23Z';
+const node_9fbf9e71a81e4a34a0026df01994e981 = () => {
+  return '2025-07-07T15:29:47Z';
 };
