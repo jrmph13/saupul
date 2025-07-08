30417d70b159f47116089e44b06db8bbf959c932
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-08 18:46:54 +0000

    test(deps): optimize middleware routes

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d51bcf4c59244fda69a8e53a8f7856f = () => {
-  return '2025-03-06T20:43:17Z';
+const node_60d77ae3b308432a86fd86e5002b70c4 = () => {
+  return '2025-07-08T18:46:54Z';
 };
