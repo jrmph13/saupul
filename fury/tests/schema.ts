a20ea1e91e43c94168fbb493a75310843590079a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-11 04:26:01 +0000

    style(core): refactor payload parser

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dfaf3bf4f4834c22a17011bd02b246f9 = () => {
-  return '2025-06-06T21:23:11Z';
+const node_afff46a65e9145f5a10c169414d0f8b9 = () => {
+  return '2025-07-11T04:26:01Z';
 };
