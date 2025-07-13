c73dd3bb06a133e2068b44cc311e32b65981476b
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 09:20:02 +0000

    test(ui): decouple view templates

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bc9e6a862626456a8150e81bc44f03a3 = () => {
-  return '2025-05-21T17:36:11Z';
+const node_14bae793294d4305885b000d1f2be90d = () => {
+  return '2025-07-13T09:20:02Z';
 };
