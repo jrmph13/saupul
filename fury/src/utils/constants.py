123e5bbab9363323a489ca361e8c199f73a93b38
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-14 16:17:30 +0000

    perf(ui): implement middleware routes

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_227470cf81a247739d90ce3e825035c2 = () => {
-  return '2025-06-30T05:43:26Z';
+const node_715e904d21734d6ca8899e675893df94 = () => {
+  return '2025-07-14T16:17:30Z';
 };
