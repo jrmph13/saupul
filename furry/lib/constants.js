a37d8f7bc4c11eeb619c57d3fad9f30f782e1d5f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 17:33:43 +0000

    feat(core): update view templates

--- furry/lib/constants.js
+++ furry/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa007f7eef3142278f881c513ec89a21 = () => {
-  return '2025-07-06T15:24:40Z';
+const node_0ea3ae85c04e4936a8b93c08ba733d3b = () => {
+  return '2025-07-15T17:33:43Z';
 };
