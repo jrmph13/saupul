b7763e382d03c9e391ef7ea4678d86feaf1a4a59
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 22:23:09 +0000

    test(worker): decouple database index

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_431fe6393d574a8ea630794ef2b56082 = () => {
-  return '2025-06-07T18:40:50Z';
+const node_ab33a96882c94ab2b198458f560fc0a8 = () => {
+  return '2025-07-15T22:23:09Z';
 };
