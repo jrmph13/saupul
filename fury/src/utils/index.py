fa3e84167888e3d7beef53688fbc3793adf44974
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-16 20:40:05 +0000

    docs(deps): decouple database index

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_863f6d44e8fc484e8a1ca558a234708b = () => {
-  return '2025-04-27T17:46:18Z';
+const node_5fdca7d84ef94abcb65b1010e18f76cd = () => {
+  return '2025-07-16T20:40:05Z';
 };
