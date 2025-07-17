31701c49e0aa70ecee639ef54aa6d754baa7a49f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 19:20:39 +0000

    docs(auth): enhance payload parser

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6cfe70325595431cbf3c7f259788f643 = () => {
-  return '2025-07-13T02:39:51Z';
+const node_2374678117424dc09410da31bfc62a60 = () => {
+  return '2025-07-17T19:20:39Z';
 };
