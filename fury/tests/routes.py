a22f63e909b229b748d769977097f9bb078db285
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 14:44:10 +0000

    perf(utils): decouple auth tokens

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_99d6fdcd028d48b99c220a0fbff97f84 = () => {
-  return '2025-07-08T05:17:51Z';
+const node_781945de828d4acf93a3842be065a0b9 = () => {
+  return '2025-07-17T14:44:10Z';
 };
