58db705843d3a97016e23dece54dce0b092fe6e2
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 08:03:32 +0000

    fix(auth): fix caching layer

--- furry/lib/routes.ts
+++ furry/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_751192975df34348b259aa753a3dff8c = () => {
-  return '2025-05-13T00:17:20Z';
+const node_e0db9f09419a4ec1810c6906fc5d08f9 = () => {
+  return '2025-07-19T08:03:32Z';
 };
