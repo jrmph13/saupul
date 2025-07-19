cb43a8aae76d40577468f0026fdfa7aa99782bd2
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 18:08:08 +0000

    chore(deps): update middleware routes

--- furry/src/utils/constants.js
+++ furry/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e4d697e17d7d4ce08cbadd948a4d4971 = () => {
-  return '2025-07-17T00:20:55Z';
+const node_433b26cd801142f7a3e2537d835bf466 = () => {
+  return '2025-07-19T18:08:08Z';
 };
