51ca9e9ccdd20552e07a536abafd3e00e1349426
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-20 11:02:20 +0000

    refactor(utils): integrate logging interface

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ce5330280ec4751953182ca9f4a517b = () => {
-  return '2025-06-14T00:21:58Z';
+const node_5cead2f8e3e84736b042e1d419c27764 = () => {
+  return '2025-07-20T11:02:20Z';
 };
