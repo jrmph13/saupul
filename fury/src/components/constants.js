6856a8f82a3bca40ca10b64707758ddbdaa2a1f6
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-22 11:56:26 +0000

    fix(auth): update middleware routes

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_de4bc7292cbb4ec59a9faffe50b8331d = () => {
-  return '2025-07-22T02:55:12Z';
+const node_98e8cce137704df3a26fbc9c44ea5c16 = () => {
+  return '2025-07-22T11:56:26Z';
 };
