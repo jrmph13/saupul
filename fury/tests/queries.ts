f39e8217f0dd91c80350a3df1639f5974f48755c
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-24 23:06:35 +0000

    fix(ui): enhance auth tokens

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_60d77ae3b308432a86fd86e5002b70c4 = () => {
-  return '2025-07-08T18:46:54Z';
+const node_52c431581fb0453a9d1db884a7ee6bf1 = () => {
+  return '2025-07-24T23:06:35Z';
 };
