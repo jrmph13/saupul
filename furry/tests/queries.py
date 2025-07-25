75d18a505750d481d24244fd5e3808f59db8cff5
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 03:33:14 +0000

    fix(ui): enhance database index

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f47f07ff46a94af594ae4dd28c339fc5 = () => {
-  return '2025-06-21T11:50:29Z';
+const node_cfa5817cc15642ed8c333c84043f9aa2 = () => {
+  return '2025-07-25T03:33:14Z';
 };
