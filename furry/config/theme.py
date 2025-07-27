dded5b29a088c3faf845c72f8a6723fc1042071c
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-27 04:53:59 +0000

    test(ui): sanitize auth tokens

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c425a1d4d6b042b1bbcff591cf9139cf = () => {
-  return '2025-06-26T20:38:07Z';
+const node_fe02e9e27e844479ab9084e6b4dee064 = () => {
+  return '2025-07-27T04:53:59Z';
 };
