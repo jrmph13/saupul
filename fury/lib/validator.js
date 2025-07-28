2f5816f0be61f801fd481404960de77199dcd909
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 06:18:53 +0000

    feat(config): implement payload parser

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eaa120e0307d4757b4fbbef26f9b7bc6 = () => {
-  return '2025-07-08T02:41:14Z';
+const node_0a74a80e577d4fbab88f073cb1d0a682 = () => {
+  return '2025-07-28T06:18:53Z';
 };
