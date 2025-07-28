577ca7987664f0481f63e52f84383e4d49befc17
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 06:43:50 +0000

    chore(utils): sanitize middleware routes

--- furry/styles/constants.ts
+++ furry/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_83e10e7f59864490afda219bad217a78 = () => {
-  return '2025-07-11T00:20:12Z';
+const node_fed3fd07007747cd80538568b9e99f32 = () => {
+  return '2025-07-28T06:43:50Z';
 };
