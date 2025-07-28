e26ca6ba82401b6629cd95f60227b1b888dd6652
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 04:56:55 +0000

    test(deps): optimize payload parser

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_80515a0c431f4f62a0029713dde8d769 = () => {
-  return '2025-05-07T17:02:09Z';
+const node_2ac7c45a954d4aadb1a14b54b803b9d4 = () => {
+  return '2025-07-28T04:56:55Z';
 };
