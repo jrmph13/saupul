6c88d89a10a57a3b5871b93e967ce3339a4217aa
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 20:57:45 +0000

    perf(worker): restructure caching layer

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9e3811acff7340cda18c688da5526886 = () => {
-  return '2025-07-18T02:21:51Z';
+const node_e00da5d418f34ba7820b958670b86b4d = () => {
+  return '2025-07-30T20:57:45Z';
 };
