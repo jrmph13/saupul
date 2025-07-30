0f75f07af7573d490de624c8c588f125d1a53c35
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 18:02:34 +0000

    chore(api): fix logging interface

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_048b7f25e8ba47878466bd99935e2eda = () => {
-  return '2025-07-02T06:38:30Z';
+const node_3bcb7e50ff384139846f75b89b793eb9 = () => {
+  return '2025-07-30T18:02:34Z';
 };
