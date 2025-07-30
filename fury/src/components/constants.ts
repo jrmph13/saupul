bd16e1ee62092f117e2cb49d1c882b1c76000768
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 03:21:17 +0000

    perf(config): fix view templates

--- fury/src/components/constants.ts
+++ fury/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a32ff92a7e941838645ca1dd28901f4 = () => {
-  return '2025-07-17T14:15:59Z';
+const node_a56cc351507c43749e0598ecdb31932c = () => {
+  return '2025-07-30T03:21:17Z';
 };
