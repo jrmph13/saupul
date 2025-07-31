09af28a51c44563ffcbcfd7ec3a281c91fdf4e74
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 05:32:05 +0000

    feat(utils): optimize payload parser

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_87ccdb6a72064a0eba2dd2d67f300987 = () => {
-  return '2025-07-27T03:44:40Z';
+const node_f876e3221b8d47c28fd40c1e89aa3581 = () => {
+  return '2025-07-31T05:32:05Z';
 };
