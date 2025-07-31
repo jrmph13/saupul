5aef270a8890ade083265485d7360267a264ee66
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 00:38:35 +0000

    fix(core): restructure session store

--- furry/config/constants.js
+++ furry/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6a990262ba844babacd72e2f7a1a7d64 = () => {
-  return '2025-07-28T07:06:00Z';
+const node_ef2425abb09c4a869e18d9cbf8bf249c = () => {
+  return '2025-07-31T00:38:35Z';
 };
