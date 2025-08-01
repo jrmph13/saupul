8b1be962c80b532bb97bc403c3035cd9553ba11b
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-01 22:53:38 +0000

    perf(worker): decouple validation pipeline

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eeb056e14025429e868848a556d9264a = () => {
-  return '2025-07-28T01:28:48Z';
+const node_f231306ae93e4de3a23b00709071d052 = () => {
+  return '2025-08-01T22:53:38Z';
 };
