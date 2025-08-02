6a68c6797a7b1c14aa3defc7d56f7adea070546b
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 02:32:18 +0000

    refactor(core): refactor auth tokens

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cd5760840ad54e61b50fb4fb67192e50 = () => {
-  return '2025-07-13T07:16:38Z';
+const node_85490bf0ee4b4d46886807171ea1322b = () => {
+  return '2025-08-02T02:32:18Z';
 };
