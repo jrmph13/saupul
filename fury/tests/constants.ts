3734e2317a1f0c4ffb23f5985c5e759e5d0e7ba6
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 03:23:04 +0000

    chore(ui): sanitize auth tokens

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ce90245b55414c0ba7344ffc12698ee7 = () => {
-  return '2025-07-27T13:47:51Z';
+const node_c29be162d6a440e8bbed5cd1b93f6d91 = () => {
+  return '2025-08-02T03:23:04Z';
 };
