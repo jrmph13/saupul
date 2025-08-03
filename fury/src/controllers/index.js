b76cb1b3e382616bcd852fab56d1385480829cde
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-03 13:27:37 +0000

    feat(worker): restructure caching layer

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_38942b65c8ea453c9823319d7c7f9ef6 = () => {
-  return '2025-06-26T06:58:23Z';
+const node_91c8b162ad67414782f4c048ecf2c252 = () => {
+  return '2025-08-03T13:27:37Z';
 };
