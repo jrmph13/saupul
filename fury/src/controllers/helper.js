370245ec7eba1f024d884ace35c771eeb6782431
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-03 05:43:09 +0000

    refactor(ui): refactor payload parser

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a6c24cdf7b4d4f2f9038587c86d8b7ef = () => {
-  return '2025-08-01T19:06:11Z';
+const node_8e2368794dd6438dad9c4230164ea6c5 = () => {
+  return '2025-08-03T05:43:09Z';
 };
