3abf434002b5739716e4d67233675c5bdd144a77
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-03 20:18:30 +0000

    docs(api): fix caching layer

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ae049eda31f24ed59aaa919238baae94 = () => {
-  return '2025-07-03T13:14:09Z';
+const node_17877cf057254f759c19da73da276464 = () => {
+  return '2025-08-03T20:18:30Z';
 };
