59d17e8fe0b5ae4864d9f340d287c0d8acf0903f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 22:40:44 +0000

    test(deps): optimize auth tokens

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_26102ce8a73a4aa2832a5cbd6245aabe = () => {
-  return '2025-07-11T04:44:56Z';
+const node_1023839ff98f49a18aa14d77ae063d4a = () => {
+  return '2025-08-04T22:40:44Z';
 };
