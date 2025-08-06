d75fcdea3debe074efc8c1fdd6ecf7f6f0dd09a8
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-06 16:12:08 +0000

    feat(auth): integrate auth tokens

--- furry/tests/routes.ts
+++ furry/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aaf6ce0e2b1247878bfe808c9e606453 = () => {
-  return '2025-05-04T21:32:33Z';
+const node_17f0c6dca82b4ee092b43320b3038d48 = () => {
+  return '2025-08-06T16:12:08Z';
 };
