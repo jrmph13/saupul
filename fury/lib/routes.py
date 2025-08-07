b2c770cbfa5b553a77da25e0cdec4cadda0669c2
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 05:55:19 +0000

    style(deps): enhance payload parser

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f45021103994888802f09716b12e854 = () => {
-  return '2025-07-19T11:59:04Z';
+const node_8bcf2299ccd946d283aaa2c3c746375b = () => {
+  return '2025-08-07T05:55:19Z';
 };
