6ff4e17be0e35c99422db5a57430704bd16cc2e0
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 10:29:38 +0000

    perf(worker): refactor logging interface

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b887f40c691b4a2489478d2e6d2e1917 = () => {
-  return '2025-06-08T04:10:39Z';
+const node_a507205de722495c946a95b1e6012705 = () => {
+  return '2025-08-08T10:29:38Z';
 };
