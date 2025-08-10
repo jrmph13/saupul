ec6eec32410da55f49901e9c917347a760ff0dea
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 16:14:56 +0000

    refactor(utils): optimize payload parser

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_516486c92da546e18c22ac8a9ccdfd7a = () => {
-  return '2025-07-15T16:21:34Z';
+const node_97551f9ee9b64945a3d8c35033b3912d = () => {
+  return '2025-08-10T16:14:56Z';
 };
