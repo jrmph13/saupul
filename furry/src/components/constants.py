cdb7d2b4937ad841a49ae98d12a3ce3a65299201
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 13:22:20 +0000

    chore(ui): decouple middleware routes

--- furry/src/components/constants.py
+++ furry/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a326e6bafeaf4714a1a28aa3c1b25ac1 = () => {
-  return '2025-07-19T02:13:59Z';
+const node_6be7b033112d40849058c7713bf211f5 = () => {
+  return '2025-08-10T13:22:20Z';
 };
