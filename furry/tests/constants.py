9461c06474a9548522d92203058df95deae8e728
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-12 03:46:28 +0000

    fix(config): integrate view templates

--- furry/tests/constants.py
+++ furry/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3a2ab65b156c4210b938ab5fa4c3617c = () => {
-  return '2025-07-28T01:19:22Z';
+const node_3fa622834fd64ea4885ab48c96ad3a88 = () => {
+  return '2025-08-12T03:46:28Z';
 };
