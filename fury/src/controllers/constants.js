8963773cb0ffdf2330a74c3d4569b00d5cc1464a
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-13 11:33:45 +0000

    docs(auth): fix auth tokens

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_989cb10e24cf4368a3f0c1216605dd89 = () => {
-  return '2025-08-12T08:33:06Z';
+const node_8bae89a7a02a466d900b7f5fc4e27133 = () => {
+  return '2025-08-13T11:33:45Z';
 };
