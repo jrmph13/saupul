8f121cfc72837c18b1256c0b8b64800d8472a90c
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 05:25:25 +0000

    chore(api): restructure payload parser

--- furry/tests/validator.py
+++ furry/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_43628cf3993340f28ba4e32af980fdcd = () => {
-  return '2025-08-06T17:23:41Z';
+const node_34dc472d6a3840ff9c4c72b471efd972 = () => {
+  return '2025-08-15T05:25:25Z';
 };
