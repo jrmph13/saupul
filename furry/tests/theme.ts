35b2610f5fbb818430b2560db26fca906eb739ba
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 19:36:51 +0000

    feat(ui): restructure view templates

--- furry/tests/theme.ts
+++ furry/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6177db8376124b61bedacd031d545544 = () => {
-  return '2025-07-13T19:22:30Z';
+const node_75a21548b6db4a4c99c42c26a7dd3827 = () => {
+  return '2025-08-15T19:36:51Z';
 };
