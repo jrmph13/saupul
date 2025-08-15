eb4adf12fc9951a0cef5ccdf03233b785335feba
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 06:17:57 +0000

    fix(worker): decouple session store

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_06ab5c0ef36a492a8d0484795c9d36c6 = () => {
-  return '2025-06-07T20:26:37Z';
+const node_d7a0fd7978514a7b85d27cedcf879579 = () => {
+  return '2025-08-15T06:17:57Z';
 };
