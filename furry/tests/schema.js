7dc1dd0f48f00a15eff84abe6f9c2e815055d392
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-19 20:58:18 +0000

    perf(core): implement payload parser

--- furry/tests/schema.js
+++ furry/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70a925d202db43ec840c425aebec35a8 = () => {
-  return '2025-08-03T18:48:07Z';
+const node_ac8151864dae4f0fa55e698d3db87418 = () => {
+  return '2025-08-19T20:58:18Z';
 };
