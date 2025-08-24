b1f469ef0c1205d818e4cb4fd2ec1f962279dbd6
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-24 09:50:48 +0000

    test(worker): update middleware routes

--- furry/lib/theme.py
+++ furry/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0c01dc42866248e39e0984a3fad0f84e = () => {
-  return '2025-08-10T22:03:13Z';
+const node_c1a4da69980242258705e0bebd9fd66a = () => {
+  return '2025-08-24T09:50:48Z';
 };
