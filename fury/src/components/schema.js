f5ded9b916a523045d2f5fd14f422e8bcb6064fe
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-30 17:57:38 +0000

    docs(core): fix validation pipeline

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a867cf24347f470a9cce3e25ef624554 = () => {
-  return '2025-08-10T14:35:11Z';
+const node_06c135ba84fd496e985bfbc5ea3d80ed = () => {
+  return '2025-08-30T17:57:38Z';
 };
