0397acc840dfea3e54dc216d1d7f2d0317af0df3
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-30 16:38:49 +0000

    feat(config): implement auth tokens

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9875c9cb58c5434e94b9ed94e00d354c = () => {
-  return '2025-04-30T00:48:06Z';
+const node_346fb1cf5a274599aa8c04edbb3fa9e2 = () => {
+  return '2025-08-30T16:38:49Z';
 };
