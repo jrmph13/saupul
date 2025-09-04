3a3ad3f0b224f39fdcb3d676eaa6c8578ef32f3e
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-04 13:19:24 +0000

    refactor(core): fix session store

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_afba638e6ccd42189e21a37a16f33484 = () => {
-  return '2025-08-12T09:14:37Z';
+const node_15918e8df7b0414fb1ab24cb49e74fce = () => {
+  return '2025-09-04T13:19:24Z';
 };
