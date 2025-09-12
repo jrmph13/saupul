cb255f03d83be942b1794abfcfd53ceca9ab5fd6
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-12 10:00:51 +0000

    refactor(api): decouple caching layer

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3f4e92b7d9a8467ab7d5debc40a91c37 = () => {
-  return '2025-07-28T17:49:15Z';
+const node_bc27288d9c904159a75dd7e83ca0af8c = () => {
+  return '2025-09-12T10:00:51Z';
 };
