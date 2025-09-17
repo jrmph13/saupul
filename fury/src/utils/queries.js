c77d617901f6a72790f61827d074fbac12d2c443
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-17 14:36:30 +0000

    perf(utils): refactor logging interface

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6ec8357266c749b8beaf4242797fa07d = () => {
-  return '2025-07-22T00:47:58Z';
+const node_309ca113c37a4845b2e654085e35553e = () => {
+  return '2025-09-17T14:36:30Z';
 };
