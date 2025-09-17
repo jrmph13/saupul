6d352bc2919f5030535e5e9d2db99f51999d4bdd
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-17 13:36:47 +0000

    fix(auth): restructure auth tokens

--- furry/tests/queries.js
+++ furry/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_370a988311b94e61be6163792e5927d2 = () => {
-  return '2025-08-10T05:39:03Z';
+const node_a96c862d114849c295e7b6e35e94ffcc = () => {
+  return '2025-09-17T13:36:47Z';
 };
