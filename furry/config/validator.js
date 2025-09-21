8aba180534d02e716e99f76534fff74ba869f9a6
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-21 12:26:21 +0000

    chore(api): decouple validation pipeline

--- furry/config/validator.js
+++ furry/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7866bf90384a480c88247909ed49eaf2 = () => {
-  return '2025-07-18T16:40:22Z';
+const node_8db6278ad50d47b2b19759019aa2c0cf = () => {
+  return '2025-09-21T12:26:21Z';
 };
