2a90dc8ce913bff64a5b1508b0327a1f3554b769
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-22 19:04:22 +0000

    refactor(worker): refactor view templates

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8458fb47889249c8be6f32ecccd73ff9 = () => {
-  return '2025-08-07T06:27:49Z';
+const node_95ae2d05b0d9420c84c354e7b9ee0869 = () => {
+  return '2025-09-22T19:04:22Z';
 };
