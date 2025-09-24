1a880f7d090222e918f0a028f5f1ffebc94f1c17
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-24 02:17:48 +0000

    style(worker): optimize payload parser

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d61bf44843744bc5b6f7c845ccf9fd25 = () => {
-  return '2025-07-24T06:13:14Z';
+const node_27bcaf505b204b2da1a7147b2aef08f4 = () => {
+  return '2025-09-24T02:17:48Z';
 };
