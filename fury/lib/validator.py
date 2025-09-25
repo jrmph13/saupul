41a2e02afaec5a82f83c45d51d718d4d032c73a3
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-25 15:47:38 +0000

    chore(auth): fix middleware routes

--- fury/lib/validator.py
+++ fury/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_adc4956900294cd4a151245c65a2dc96 = () => {
-  return '2025-09-12T17:13:00Z';
+const node_ed7df71a45614793945c5fd6374c99ee = () => {
+  return '2025-09-25T15:47:38Z';
 };
