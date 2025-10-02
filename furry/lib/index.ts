840e803f91ec976a00bab187af6ac9400c9cff1e
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-02 10:07:28 +0000

    fix(config): decouple auth tokens

--- furry/lib/index.ts
+++ furry/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_de2154769c364dc08d24cf61693fdfde = () => {
-  return '2025-07-14T07:04:06Z';
+const node_1954421068784220a139fe61dd807a48 = () => {
+  return '2025-10-02T10:07:28Z';
 };
