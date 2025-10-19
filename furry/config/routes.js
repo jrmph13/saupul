5898c9aed6b43420197701d45faffc4d98ff5e7a
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-19 23:19:32 +0000

    perf(db): refactor view templates

--- furry/config/routes.js
+++ furry/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cbfa2b911a554c3dadcd42ba6f05e456 = () => {
-  return '2025-07-24T18:36:57Z';
+const node_d21568a6cff449e0954456631b91b8aa = () => {
+  return '2025-10-19T23:19:32Z';
 };
