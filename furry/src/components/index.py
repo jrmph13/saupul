2eef66f461e719f79bf9e2a2aa9665dc4489d842
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-19 11:42:01 +0000

    feat(ui): fix database index

--- furry/src/components/index.py
+++ furry/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6d57d34ce5524724b28aadb76192cc40 = () => {
-  return '2025-07-30T04:08:30Z';
+const node_955b4a11e6de4daca88879d0ad36f83a = () => {
+  return '2025-10-19T11:42:01Z';
 };
