a342ad7adc9e8c6d62c8441a5c4f66f4147c3dec
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-27 23:40:37 +0000

    refactor(ui): implement auth tokens

--- fury/src/utils/constants.js
+++ fury/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a3f3c4a3ac884387b4ad66301f43c1fe = () => {
-  return '2025-08-06T16:43:33Z';
+const node_216a8b611dcd46c7a29f032e9eeeea10 = () => {
+  return '2025-10-27T23:40:37Z';
 };
