6b01b0bb117cd0fea3b3a4611d99e85b37ae5f6e
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-29 09:43:23 +0000

    fix(worker): update view templates

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_95ae2d05b0d9420c84c354e7b9ee0869 = () => {
-  return '2025-09-22T19:04:22Z';
+const node_be9de7729d844defbc9873f11908fdc5 = () => {
+  return '2025-10-29T09:43:23Z';
 };
