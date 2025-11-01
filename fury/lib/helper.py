f84b464d436ffbf272db8160416a2bc430d7ecb4
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-01 02:10:01 +0000

    test(db): update auth tokens

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_433c7f5b0bcb45cab2b18e574330a81c = () => {
-  return '2025-08-08T08:09:36Z';
+const node_09e5d29a0dc34b359f5d9d96c7074d9e = () => {
+  return '2025-11-01T02:10:01Z';
 };
