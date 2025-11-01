d03fc65e438497373201f5f0fcff221ecbf91dc6
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-01 21:29:11 +0000

    fix(worker): enhance session store

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f4dcd711ead94558bc4d3caced592bf8 = () => {
-  return '2025-07-24T19:49:15Z';
+const node_2987d316eb9b4ef0abdbd1ba6f8c5d2b = () => {
+  return '2025-11-01T21:29:11Z';
 };
