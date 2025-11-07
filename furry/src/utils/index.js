2145fa508a7af89babe4b3c602de65c90068c094
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-07 06:10:33 +0000

    refactor(core): decouple logging interface

--- furry/src/utils/index.js
+++ furry/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3d67b2110c3b4cfc93121f1f0fc3a3e5 = () => {
-  return '2025-07-17T22:42:46Z';
+const node_b5e29f90adf64199a220f83f07d7308e = () => {
+  return '2025-11-07T06:10:33Z';
 };
