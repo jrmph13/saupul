a217ce157120629c0f08de00727865d66390dad2
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-07 10:52:20 +0000

    docs(api): fix view templates

--- furry/tests/routes.ts
+++ furry/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_17f0c6dca82b4ee092b43320b3038d48 = () => {
-  return '2025-08-06T16:12:08Z';
+const node_cea85d81ad734bbdb73e845522b3137d = () => {
+  return '2025-11-07T10:52:20Z';
 };
