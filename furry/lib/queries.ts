4c60d0d8637b485c8214127991ea669000520a3d
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-09 23:58:57 +0000

    style(db): restructure session store

--- furry/lib/queries.ts
+++ furry/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fa48c23d0f314c3dae19db3556104cbf = () => {
-  return '2025-11-07T17:06:25Z';
+const node_13eb619179f74a849c1a75b1d91dc165 = () => {
+  return '2025-11-09T23:58:57Z';
 };
