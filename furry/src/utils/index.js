d71b853f4d559869ba5796e94da845636973f116
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-13 01:30:05 +0000

    docs(auth): implement payload parser

--- furry/src/utils/index.js
+++ furry/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b5e29f90adf64199a220f83f07d7308e = () => {
-  return '2025-11-07T06:10:33Z';
+const node_1fd613c7087c48008a7bb2100d0c9006 = () => {
+  return '2025-11-13T01:30:05Z';
 };
