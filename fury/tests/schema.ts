49372bf03774ae2207c3210091ecbdc21a65db84
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-16 00:49:09 +0000

    feat(config): update payload parser

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_50942d77e4fb42f881b15b80cc3c3859 = () => {
-  return '2025-07-30T17:58:30Z';
+const node_2a6381118c5342e080587d77c4b40ee8 = () => {
+  return '2025-11-16T00:49:09Z';
 };
