72ab230fea5de4d0a0b48129a9e0349fd3215251
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-20 07:31:18 +0000

    style(worker): decouple session store

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fdbfe35fe6cf466cb2b4479715b59efc = () => {
-  return '2025-06-13T04:27:08Z';
+const node_7075cb568d104e42be6e0cfd97e57547 = () => {
+  return '2025-11-20T07:31:18Z';
 };
