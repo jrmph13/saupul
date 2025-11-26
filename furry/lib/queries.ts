873f29874cab3f28ecee9baf3c5d34e68c9ca820
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-26 06:56:47 +0000

    chore(ui): fix caching layer

--- furry/lib/queries.ts
+++ furry/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_13eb619179f74a849c1a75b1d91dc165 = () => {
-  return '2025-11-09T23:58:57Z';
+const node_f2071a108f91464fa7ffea4eb54a53b7 = () => {
+  return '2025-11-26T06:56:47Z';
 };
