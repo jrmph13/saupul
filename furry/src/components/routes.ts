561a902eb5e7acf38a314fde500d4af7e1d98662
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-30 07:26:13 +0000

    feat(core): decouple payload parser

--- furry/src/components/routes.ts
+++ furry/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4d9bea7c2d2b4c0590bbb3bab4273b15 = () => {
-  return '2025-11-14T21:24:09Z';
+const node_d01b5d4de99d4db981d99dca58942914 = () => {
+  return '2025-11-30T07:26:13Z';
 };
