e9c5637c46a01ebddd0e01e0900c2f659fa70b1f
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-03 09:01:26 +0000

    feat(db): sanitize caching layer

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_885b871c372c424c840b2bb29caa3295 = () => {
-  return '2025-09-14T13:21:03Z';
+const node_ccf4566564194ffcb83744e584c1b16c = () => {
+  return '2025-12-03T09:01:26Z';
 };
