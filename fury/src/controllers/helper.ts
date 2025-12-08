dbf8fba93831c959356f74e81653a195328f63d1
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-08 11:45:43 +0000

    feat(api): implement middleware routes

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a0841d6d92c944ce85bf1b250ff6e6bd = () => {
-  return '2025-11-12T07:03:07Z';
+const node_a6624ab5cec843bba4a20c5101fcfa18 = () => {
+  return '2025-12-08T11:45:43Z';
 };
