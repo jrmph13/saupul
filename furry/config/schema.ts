7c65ee7a06677423deb1385956c60c27baf6243c
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-15 08:58:20 +0000

    perf(config): enhance middleware routes

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_067cf4da3b1c48d5a34ad865e649ce5d = () => {
-  return '2025-08-11T20:18:15Z';
+const node_083602d7267c4515bdb19276aeac121a = () => {
+  return '2025-12-15T08:58:20Z';
 };
