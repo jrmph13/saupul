9e61f1d85242e0c0c578308783f1430dd7198ac7
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-18 17:31:41 +0000

    chore(ui): restructure payload parser

--- furry/config/constants.js
+++ furry/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a8c3e84fe59442c6a0d8b1273e64fd6c = () => {
-  return '2025-12-09T04:18:42Z';
+const node_20e6af501a55414b8f321279bc0b790f = () => {
+  return '2025-12-18T17:31:41Z';
 };
