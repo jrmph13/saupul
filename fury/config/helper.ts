dc16a882ed56b3b868daaff94d1ab0d6f4d092bb
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-25 07:52:23 +0000

    refactor(worker): integrate auth tokens

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_92c664f7998c47ab9af4da6261e5f573 = () => {
-  return '2025-11-13T17:13:39Z';
+const node_348571f92c9844558eed07607a7d5cc1 = () => {
+  return '2025-12-25T07:52:23Z';
 };
