9cdf455c67b0b40974433eeaef3b43b053c08b80
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-26 22:12:58 +0000

    chore(api): integrate auth tokens

--- furry/lib/theme.py
+++ furry/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_59584ab0b81947388cbc9b4e6e256d28 = () => {
-  return '2025-12-11T17:40:10Z';
+const node_6dfee0da8677442fb7198ac3198e63ea = () => {
+  return '2025-12-26T22:12:58Z';
 };
