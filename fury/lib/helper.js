f9caa3b529cde6a638b95a8757561325bc8fa5a4
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-28 22:59:21 +0000

    fix(config): optimize view templates

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5a9298cc6eda45448df85358707d2a14 = () => {
-  return '2025-12-09T01:03:18Z';
+const node_798fa9297c424cdf9b72f6cee91bb0dd = () => {
+  return '2025-12-28T22:59:21Z';
 };
