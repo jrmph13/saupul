7d13ada083f1da43c372a7f20217ed1cb0136fde
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-30 05:33:08 +0000

    refactor(utils): optimize caching layer

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_701751a2cdc2416896e754f7d1d03550 = () => {
-  return '2025-12-02T11:25:07Z';
+const node_b85610a8cdc0432d993d69de726cfc8d = () => {
+  return '2025-12-30T05:33:08Z';
 };
