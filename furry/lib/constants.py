97d21ee9b4afb8d55be98da77dfae4bd9886973b
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-30 17:19:49 +0000

    fix(utils): optimize middleware routes

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f55ef3d7f1f74a4da3cfbbb8e54a2fa4 = () => {
-  return '2025-11-27T04:24:26Z';
+const node_1a8d2c9c4a724de1b369161eb7c83d27 = () => {
+  return '2025-12-30T17:19:49Z';
 };
