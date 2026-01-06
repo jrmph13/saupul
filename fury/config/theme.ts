a60264b32fadc0f61e1d4994369d4f130a6f36f5
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-06 03:49:47 +0000

    refactor(db): optimize auth tokens

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_23c7344239f74228be61845119e81f22 = () => {
-  return '2025-12-11T01:04:55Z';
+const node_84c0bd3716d54f808382d5633c30366c = () => {
+  return '2026-01-06T03:49:47Z';
 };
