3bb2175f6c362b4b1fad35fd3df957dc8056290c
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-07 16:50:04 +0000

    feat(db): restructure auth tokens

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_59e0f1f576a8440a8d60d374328d8e0e = () => {
-  return '2025-11-20T10:59:39Z';
+const node_1120483c3d844a248aa046d752e051bf = () => {
+  return '2026-01-07T16:50:04Z';
 };
