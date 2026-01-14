ae5f854f1828b0c9a4f3297e1e1170a20c29ab88
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-14 11:43:40 +0000

    docs(ui): sanitize view templates

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_145e3e70ba8a464a80cae95c1095426c = () => {
-  return '2025-09-26T13:41:26Z';
+const node_8ca48eb069c8485faa8135d3e80cdfe0 = () => {
+  return '2026-01-14T11:43:40Z';
 };
