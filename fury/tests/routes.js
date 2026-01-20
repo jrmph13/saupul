78566791a92d131805b89ffe50362e2d04995bac
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-20 00:35:23 +0000

    feat(ui): restructure middleware routes

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9acd0541e665467d9c3225fb8ca915b4 = () => {
-  return '2025-11-27T00:18:54Z';
+const node_e64da2a82c224f1e83b7e8d39d2272db = () => {
+  return '2026-01-20T00:35:23Z';
 };
