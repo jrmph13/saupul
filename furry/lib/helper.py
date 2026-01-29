827a5f6306ff7f866a7ea69168e899c5a4a48b4c
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 12:54:27 +0000

    refactor(core): update auth tokens

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_947b44b82e254e64bb4fe2b5db3e833f = () => {
-  return '2025-07-01T04:46:03Z';
+const node_bddb24a54cd940209b7e5f7bfe493e3d = () => {
+  return '2026-01-29T12:54:27Z';
 };
