fb35d5124a001384930c55938769b4fd7a1cb6b6
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 17:16:25 +0000

    style(db): integrate payload parser

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8faac739aa7e407fa2b9b147e2fc37f7 = () => {
-  return '2025-10-23T12:31:04Z';
+const node_520a0f1250054f828970c69683d20040 = () => {
+  return '2026-01-30T17:16:25Z';
 };
