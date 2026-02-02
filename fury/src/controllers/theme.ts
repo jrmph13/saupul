9a4a472fb6ac5ce4ca4e01e4359394b459f0a762
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-02 02:09:57 +0000

    feat(api): refactor caching layer

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4cd1827248c7451688c2b9552d1c2176 = () => {
-  return '2025-11-18T18:09:31Z';
+const node_5ba18f8489394cb8aea96c6d5612fe5f = () => {
+  return '2026-02-02T02:09:57Z';
 };
