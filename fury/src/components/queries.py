808f33ccd71c1382ba64608cd2ffacfb9e041144
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-02 05:21:41 +0000

    test(db): fix auth tokens

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_540d9d18b8d54a90b49f45994d887c80 = () => {
-  return '2025-07-28T22:27:04Z';
+const node_c9c42011f03b4466bdb8a9adf3b01baa = () => {
+  return '2026-02-02T05:21:41Z';
 };
