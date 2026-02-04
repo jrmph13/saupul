a42ebff10c54fd5bc48d804351154d5c6ab6fd3b
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 02:03:45 +0000

    feat(db): refactor middleware routes

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f132c8e498e74b6098029969cd16390d = () => {
-  return '2025-09-24T08:23:04Z';
+const node_a66143334f204908bb0bec955c0a8587 = () => {
+  return '2026-02-04T02:03:45Z';
 };
