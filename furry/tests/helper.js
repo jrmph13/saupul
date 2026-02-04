db6d01d26ad005ca016ae13c35f5c5b428a871a7
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 07:01:32 +0000

    docs(core): sanitize view templates

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1cc49f3a55c74ac6b4fafd4851f551ca = () => {
-  return '2025-11-25T23:00:04Z';
+const node_6cde4f0592874469a4be0044ed04953a = () => {
+  return '2026-02-04T07:01:32Z';
 };
