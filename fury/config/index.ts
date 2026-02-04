a08e1dc85e36c9c4effeb3f825edda998348e6bf
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 17:32:46 +0000

    chore(auth): update caching layer

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_df89ecd854684afa847201856a2869bf = () => {
-  return '2025-10-26T12:10:55Z';
+const node_ff9fd038b60a4845a74c7fcbbf8886d4 = () => {
+  return '2026-02-04T17:32:46Z';
 };
