e5c358449706a16d4289027f62da81088899ebaa
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-06 14:55:38 +0000

    test(worker): integrate database index

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_83934171cfe8400ea21d566424fe9998 = () => {
-  return '2025-12-19T22:06:52Z';
+const node_03a63222beec44a8b8255a553152487d = () => {
+  return '2026-02-06T14:55:38Z';
 };
