01cedbf329a7eec00a930414dbc8c28b5e7a21ac
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-10 20:56:48 +0000

    refactor(db): decouple middleware routes

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cbd1d2a867c648d994c9121d5eebeaff = () => {
-  return '2026-02-09T11:26:41Z';
+const node_65f61d960237497c9b58807966381104 = () => {
+  return '2026-02-10T20:56:48Z';
 };
