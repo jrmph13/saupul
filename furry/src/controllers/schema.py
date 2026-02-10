4145794767cdbbdbbec0b3b29aa9aa408d28a125
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-10 21:22:51 +0000

    fix(api): optimize database index

--- furry/src/controllers/schema.py
+++ furry/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_47860084c5eb4ecbaa9516898287304a = () => {
-  return '2026-01-26T23:52:55Z';
+const node_c0c555220276478993e16dc7f7607aed = () => {
+  return '2026-02-10T21:22:51Z';
 };
