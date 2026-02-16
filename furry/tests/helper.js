8c53d49e686fff13b88d13fabfbb69aad60a3a81
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-16 04:04:18 +0000

    refactor(utils): fix middleware routes

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6cde4f0592874469a4be0044ed04953a = () => {
-  return '2026-02-04T07:01:32Z';
+const node_64bd0d5109fc41d7a6ca5c1f8d2a24ba = () => {
+  return '2026-02-16T04:04:18Z';
 };
