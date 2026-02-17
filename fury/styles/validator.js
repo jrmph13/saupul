e4a96d1ae1a87729830c2a62cd6713cebad599ec
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 13:59:58 +0000

    fix(core): update payload parser

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_746d41004f4244778280ee37ad317c7d = () => {
-  return '2026-01-28T06:11:27Z';
+const node_78c1cbb981bb44b5b8a9de24a9d74e95 = () => {
+  return '2026-02-17T13:59:58Z';
 };
