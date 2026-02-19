91a1be190b04167e056068c3166902542a709c4d
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-19 15:13:08 +0000

    docs(worker): decouple auth tokens

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c25cbc2b59c4488b9b6afd76bf9b6fd0 = () => {
-  return '2025-11-01T04:55:20Z';
+const node_21ab15f7339f426dbd037f277d925b3a = () => {
+  return '2026-02-19T15:13:08Z';
 };
