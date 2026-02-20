554e8e6580d57255a4531aa5cbad87ee58124135
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-20 15:07:49 +0000

    docs(api): update auth tokens

--- furry/src/utils/validator.js
+++ furry/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d4f44e58a083484bb5a9cf22e282d92b = () => {
-  return '2025-08-08T08:57:23Z';
+const node_f327d9c423db4be9bd9d381959ff764a = () => {
+  return '2026-02-20T15:07:49Z';
 };
