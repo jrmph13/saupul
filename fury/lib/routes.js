57bd54ce7ef19641afa8d3903cb994787d1f8091
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-23 04:03:58 +0000

    test(core): optimize middleware routes

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_78e13f65c8d34def8f199922be422be9 = () => {
-  return '2026-02-13T04:55:59Z';
+const node_54827709fcda4c108490ba077bbbe85c = () => {
+  return '2026-02-23T04:03:58Z';
 };
