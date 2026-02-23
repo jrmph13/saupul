d4e0df941b082c44929451756d2af4d904ab400c
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-23 16:46:39 +0000

    fix(ui): optimize auth tokens

--- furry/src/utils/index.js
+++ furry/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3d3aba99c51348178047b1fdb0278d95 = () => {
-  return '2025-11-30T21:31:00Z';
+const node_4bc82906e56746fa9da94000a88a9924 = () => {
+  return '2026-02-23T16:46:39Z';
 };
