9741a495bccc319c83a6c7b52bca2b63e1b415d8
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-26 07:32:17 +0000

    fix(ui): restructure auth tokens

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_55f9b71276414862993b558eca013714 = () => {
-  return '2025-11-28T04:16:20Z';
+const node_c6a27af4bb02405f889892046435d779 = () => {
+  return '2026-02-26T07:32:17Z';
 };
