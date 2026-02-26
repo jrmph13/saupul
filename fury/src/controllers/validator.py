044c51da78524467744dda0f5273c21786e064a5
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-26 07:33:16 +0000

    fix(utils): restructure middleware routes

--- fury/src/controllers/validator.py
+++ fury/src/controllers/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c3401873dc704a88a14550d6aa94a5c2 = () => {
-  return '2026-01-27T09:02:00Z';
+const node_a5d55744519f40c1b377f1a10b4eafbf = () => {
+  return '2026-02-26T07:33:16Z';
 };
