5b71826112ee612100823946780856288c588414
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-05 04:56:47 +0000

    chore(auth): refactor caching layer

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a73745b5c67c4d16866db34eb35ef025 = () => {
-  return '2026-02-22T14:57:19Z';
+const node_e996302e66584317852efaf29cf46b8d = () => {
+  return '2026-03-05T04:56:47Z';
 };
