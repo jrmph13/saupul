e9bd3c4f6bc526f11f955d25813cfb3c9127e781
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-05 12:53:43 +0000

    feat(ui): update middleware routes

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6c029a96894b4e4499ef441b081027de = () => {
-  return '2026-02-02T05:37:50Z';
+const node_b41dcdc238cf4b82badc17aac7e155cc = () => {
+  return '2026-03-05T12:53:43Z';
 };
