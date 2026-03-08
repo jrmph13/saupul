2872cfb00bb5cc998b8208f41de60ebce0d53f30
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 01:30:17 +0000

    fix(config): refactor session store

--- furry/styles/routes.js
+++ furry/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6ae84ddf43964c41bd7369734ce99c5d = () => {
-  return '2025-12-15T12:03:37Z';
+const node_baedfc2f690b4d919c27c4714c7967a3 = () => {
+  return '2026-03-08T01:30:17Z';
 };
