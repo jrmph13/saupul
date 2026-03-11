f20c6f7dde1eb82ce297cbc447d05b195b8b570d
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-11 07:24:45 +0000

    perf(utils): refactor auth tokens

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f3e42425c2c045198a68330aa4fe158e = () => {
-  return '2025-12-17T00:39:22Z';
+const node_944578eee83140078267baa15c9c1ce8 = () => {
+  return '2026-03-11T07:24:45Z';
 };
