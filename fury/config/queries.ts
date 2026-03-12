bb9f2002e66058a1259b9fb6ec473459da3b5de7
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-12 15:13:58 +0000

    fix(utils): decouple payload parser

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_29ace30157fd466588b70d12465a45ef = () => {
-  return '2026-03-06T13:51:42Z';
+const node_af7410aaabca442fb8a700c3a1625a78 = () => {
+  return '2026-03-12T15:13:58Z';
 };
