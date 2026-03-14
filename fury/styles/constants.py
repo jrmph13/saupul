42f28b9e7eb8f825b7f92f202dd762e2ba47fb96
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 11:21:33 +0000

    perf(core): decouple auth tokens

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_afc3e917e9314a99a3ae557bfb785c48 = () => {
-  return '2026-02-18T02:39:56Z';
+const node_17ec3ad104ad49669fc49dba73572b63 = () => {
+  return '2026-03-14T11:21:33Z';
 };
