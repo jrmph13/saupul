c04395a44854cdaebdbc4ab414697ccf5bb67152
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 16:11:39 +0000

    chore(auth): restructure view templates

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5a333b02b9e440868bb7104b2ce40b03 = () => {
-  return '2026-03-05T12:46:02Z';
+const node_2bf6c70db5174f4da0f9b6c7767ff5a4 = () => {
+  return '2026-03-14T16:11:39Z';
 };
