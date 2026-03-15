53449b0f0105fb74d9616669880359db86993fd0
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-15 08:56:49 +0000

    perf(deps): fix payload parser

--- furry/src/controllers/schema.ts
+++ furry/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_03363d691ac44462ae170a45ed269255 = () => {
-  return '2026-02-27T00:26:38Z';
+const node_53c2e970f1c24923ad74e9e7e90f3310 = () => {
+  return '2026-03-15T08:56:49Z';
 };
