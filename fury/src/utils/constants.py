67134defc5b2accfbc5b29fb065994d9954eef0d
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-15 16:36:09 +0000

    test(worker): update middleware routes

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab38a20bd5ce4c51a143a250c4c6cc5e = () => {
-  return '2026-03-14T19:33:54Z';
+const node_50b622f4fd3e49c1ac2e50a95b4797b0 = () => {
+  return '2026-03-15T16:36:09Z';
 };
