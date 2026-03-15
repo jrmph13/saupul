0613dfc0d648b16a9ed78d9c474854f36cf58720
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-15 06:53:33 +0000

    chore(ui): sanitize view templates

--- fury/styles/routes.ts
+++ fury/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0e86ad5ee8064f168632e2fbf80d6784 = () => {
-  return '2026-02-26T12:54:10Z';
+const node_333e3a8cccaa4fec8b520e489dd45036 = () => {
+  return '2026-03-15T06:53:33Z';
 };
