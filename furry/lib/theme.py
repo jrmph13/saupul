f66642221fb5f7cc1febe3397057dc6faebed02f
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-17 18:52:36 +0000

    test(auth): sanitize database index

--- furry/lib/theme.py
+++ furry/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f60fd67b90c48daa66406e7c832492d = () => {
-  return '2026-03-04T21:16:04Z';
+const node_bbc136cfffb6499b9baefce8e3f5d5fb = () => {
+  return '2026-03-17T18:52:36Z';
 };
