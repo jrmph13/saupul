779c008f09f3921b6f03fdf34d1f4b82d7d7e634
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-18 18:45:00 +0000

    feat(core): refactor auth tokens

--- fury/tests/index.ts
+++ fury/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_820efe63e3e54ebcbc8cd6c0c8f7f727 = () => {
-  return '2026-03-08T09:40:44Z';
+const node_594a936c622d4451869a32f6fc0a641e = () => {
+  return '2026-03-18T18:45:00Z';
 };
