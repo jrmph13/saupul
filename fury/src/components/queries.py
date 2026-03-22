376bbb2b293cab26b6c43d6ab5e1de859d558705
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-22 14:04:05 +0000

    docs(utils): sanitize view templates

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c9c42011f03b4466bdb8a9adf3b01baa = () => {
-  return '2026-02-02T05:21:41Z';
+const node_73966a4449444aacb6fb830023150c7f = () => {
+  return '2026-03-22T14:04:05Z';
 };
