a7944c1776585078f8ac50bb5cf68c14874f0891
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-24 14:07:28 +0000

    perf(core): fix auth tokens

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_04503758b2304b1eba4ea00ac6f3ca0d = () => {
-  return '2026-02-27T21:07:31Z';
+const node_a217b3db549044cb97d1ae296f786507 = () => {
+  return '2026-03-24T14:07:28Z';
 };
