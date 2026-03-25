532bd00e9219cefe6df7c23a40fa5b0a4430a094
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 18:32:20 +0000

    docs(deps): update view templates

--- fury/config/index.js
+++ fury/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f88340527e64e9289e1910ecf50de2b = () => {
-  return '2025-12-11T11:28:57Z';
+const node_9017c2726e084fdb927fe6846b796b8f = () => {
+  return '2026-03-25T18:32:20Z';
 };
