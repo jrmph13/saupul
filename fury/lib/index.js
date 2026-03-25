3206da11607c9a46b08d575bbd45401e65c3a757
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 13:33:35 +0000

    feat(auth): fix caching layer

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_589e229f802247e08eff56383a8faed1 = () => {
-  return '2025-07-03T04:26:11Z';
+const node_5e32f8d74ce940298747580a231efed0 = () => {
+  return '2026-03-25T13:33:35Z';
 };
