667448f7b236f2426c8b488f7b022018e61d0eb6
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-26 19:27:45 +0000

    fix(worker): implement middleware routes

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e996302e66584317852efaf29cf46b8d = () => {
-  return '2026-03-05T04:56:47Z';
+const node_2464315cd586428a83235c07fc80b912 = () => {
+  return '2026-03-26T19:27:45Z';
 };
