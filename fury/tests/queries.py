ead2b4b66f8742a662016e0d44fad76e183596bc
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-29 09:17:18 +0000

    feat(ui): refactor middleware routes

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_48ba5bd19a694f1dbfe438bb218f40ad = () => {
-  return '2026-02-10T14:50:26Z';
+const node_cb38620b723e4063bb08d87d382a8fe0 = () => {
+  return '2026-03-29T09:17:18Z';
 };
