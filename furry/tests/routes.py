d3218078b19129b27f431667253123fb1cb243b7
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-31 10:26:43 +0000

    fix(worker): optimize auth tokens

--- furry/tests/routes.py
+++ furry/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ba235f18e3374ba587d3560f9b6bf522 = () => {
-  return '2026-03-14T22:40:15Z';
+const node_fd55ca641d034ea285a6f52e8f00c78f = () => {
+  return '2026-03-31T10:26:43Z';
 };
