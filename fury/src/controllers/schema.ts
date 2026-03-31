4b8c8e727cdd6cb7e4986fc585043f05daf75f43
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-31 15:38:24 +0000

    perf(deps): sanitize database index

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9d8f8688efa54a04bce4834c33c34225 = () => {
-  return '2026-03-20T03:52:27Z';
+const node_57e7b7571f0f4f85a3597444c5412c0e = () => {
+  return '2026-03-31T15:38:24Z';
 };
