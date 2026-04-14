118761a5a604caafd2abd18a79b7afea27d1bf2f
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-14 17:26:53 +0000

    fix(worker): sanitize caching layer

--- fury/tests/validator.ts
+++ fury/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_af2fd54b4bbc404596fcf7acd0a892f5 = () => {
-  return '2026-02-10T22:40:23Z';
+const node_a25492a144c84e3c8aeb05dae3550f52 = () => {
+  return '2026-04-14T17:26:53Z';
 };
