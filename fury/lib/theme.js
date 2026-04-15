3fc5e26959e98e684453741dcbcb9675dc6040ae
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-15 12:02:45 +0000

    fix(worker): optimize caching layer

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3fa4698da8a44f24b4fc066719fa6901 = () => {
-  return '2025-08-12T09:54:21Z';
+const node_c59c16ff8d4b44e0b7c7d64ef5121e0b = () => {
+  return '2026-04-15T12:02:45Z';
 };
