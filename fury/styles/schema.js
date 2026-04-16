7a5b2b2807be717d9bee24424ea1b31aef25f4c8
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-16 12:27:59 +0000

    perf(core): implement payload parser

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cb25174cbe4d4675a40e610853ff2194 = () => {
-  return '2026-04-15T00:30:47Z';
+const node_2c20a3026542435d8d9f57a7343d031f = () => {
+  return '2026-04-16T12:27:59Z';
 };
