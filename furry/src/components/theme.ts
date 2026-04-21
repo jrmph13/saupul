730884e733c10114788be6a3081abb10da90525f
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-21 01:41:12 +0000

    fix(auth): optimize session store

--- furry/src/components/theme.ts
+++ furry/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c49e078fb5f048feb28ad6710596fa0b = () => {
-  return '2026-02-06T13:09:30Z';
+const node_728599439d744d6cb4362389a245e1d1 = () => {
+  return '2026-04-21T01:41:12Z';
 };
