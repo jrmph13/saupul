6e1642f7559e675a854e3d1f97cc147e27abb0dc
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-23 16:37:51 +0000

    perf(ui): enhance middleware routes

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f97313f6550d4dacb025acc5c9e48c30 = () => {
-  return '2026-02-04T00:22:19Z';
+const node_4408859413f141e6babc6f6528abf3c2 = () => {
+  return '2026-04-23T16:37:51Z';
 };
