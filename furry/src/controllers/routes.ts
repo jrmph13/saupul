58e6ad876dd3e962dbc5d1a737e407d7dc8726ce
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-26 08:15:29 +0000

    fix(core): integrate view templates

--- furry/src/controllers/routes.ts
+++ furry/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e62627724c1f49cdbc0e500db7bfaaa8 = () => {
-  return '2026-04-22T10:59:02Z';
+const node_2197e5a5cb334adcbe5e9675aab994da = () => {
+  return '2026-04-26T08:15:29Z';
 };
