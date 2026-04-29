227ab8448978963b06de954e6a5b9b298b0523b2
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-29 15:00:23 +0000

    docs(api): restructure caching layer

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0b4182c86edd4b84b17aecdb0ab06479 = () => {
-  return '2026-03-15T20:11:38Z';
+const node_f5073496a5c8442fa585f6b07a371144 = () => {
+  return '2026-04-29T15:00:23Z';
 };
