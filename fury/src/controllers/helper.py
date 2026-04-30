e77d346622c92c600359747d6fc545a400f3745f
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-30 09:33:02 +0000

    feat(api): refactor auth tokens

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e4798b2a24cb46b3a760ca46d2eb638f = () => {
-  return '2026-04-14T02:28:00Z';
+const node_8eb0cc5594a54403973c1e9188e8b54b = () => {
+  return '2026-04-30T09:33:02Z';
 };
