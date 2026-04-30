01c37b24fff1f429b7c26befbb3afaf63c092925
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-30 21:58:03 +0000

    perf(db): enhance auth tokens

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_31551707f6fe441c93157ffdc9a4a94b = () => {
-  return '2026-03-11T07:12:38Z';
+const node_545778ff2641407ea87621ddb00ff53f = () => {
+  return '2026-04-30T21:58:03Z';
 };
