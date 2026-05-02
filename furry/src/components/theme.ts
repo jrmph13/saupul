edcd85f56ebf490667ffa8091463dde1b12d272b
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 13:45:20 +0000

    fix(core): integrate auth tokens

--- furry/src/components/theme.ts
+++ furry/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_728599439d744d6cb4362389a245e1d1 = () => {
-  return '2026-04-21T01:41:12Z';
+const node_80bd9b04eab24f3d81affccd8c83cdf3 = () => {
+  return '2026-05-02T13:45:20Z';
 };
