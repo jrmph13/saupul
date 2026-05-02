16d51ba85329fb9780f02fc6005527bb3e69e7a3
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 22:53:08 +0000

    chore(worker): decouple payload parser

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_704310b825db494e973ff5c6775eb869 = () => {
-  return '2026-04-24T18:15:58Z';
+const node_4ad2953d8f354b93aa84879673053858 = () => {
+  return '2026-05-02T22:53:08Z';
 };
