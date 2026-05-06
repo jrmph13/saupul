f2991c3f8da8f8bf97768ca88a69a8067a2cd143
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 15:03:05 +0000

    perf(auth): restructure validation pipeline

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_73c7f0ea4876402c84b4dae0fdca2482 = () => {
-  return '2026-04-29T19:06:06Z';
+const node_f68e3384c2534d4b81e8fe9c7ab6b77f = () => {
+  return '2026-05-06T15:03:05Z';
 };
