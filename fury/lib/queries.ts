710173e617a34487c7565dda070fb9eec164a64a
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-10 17:17:13 +0000

    chore(utils): refactor caching layer

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7d4745a4b3b94349a741cc24e0fc01d7 = () => {
-  return '2026-04-13T17:16:10Z';
+const node_569e392363144d2bad2c6c3af39c153a = () => {
+  return '2026-05-10T17:17:13Z';
 };
