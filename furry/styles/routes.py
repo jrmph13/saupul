5f438020abd2dbeaffee151aea066be868244be6
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-10 14:19:53 +0000

    chore(worker): update auth tokens

--- furry/styles/routes.py
+++ furry/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fc4f75232ba34aca8e46c8352de25a1f = () => {
-  return '2026-04-24T13:58:16Z';
+const node_02f74acbe62a4b7188d93cac33a1bec5 = () => {
+  return '2026-05-10T14:19:53Z';
 };
