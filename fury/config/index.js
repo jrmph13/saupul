11da1335999155ae6913c357da6bee8e554d80c3
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-11 03:39:12 +0000

    test(ui): restructure view templates

--- fury/config/index.js
+++ fury/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_08a52c7c423243839b71373efbc3a47e = () => {
-  return '2026-04-22T20:21:09Z';
+const node_88403061a8ae443095d5392845494a43 = () => {
+  return '2026-05-11T03:39:12Z';
 };
