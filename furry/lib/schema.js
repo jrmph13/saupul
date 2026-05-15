4d37a2f201d263ffc0f3b09bd564373df79e3bc5
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-15 03:06:46 +0000

    chore(db): fix payload parser

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b95e0ac00c3444c187f82beed2f7d6b2 = () => {
-  return '2026-03-04T06:42:36Z';
+const node_b210c2bf5a5543bf92b5b6a0817025e2 = () => {
+  return '2026-05-15T03:06:46Z';
 };
