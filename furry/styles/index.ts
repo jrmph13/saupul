588967c1d7561ed7db5610b5c7ae27ee91e6a635
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-15 13:56:13 +0000

    chore(worker): restructure view templates

--- furry/styles/index.ts
+++ furry/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c80c2dc96b64d9da329d3bebfaf7c8a = () => {
-  return '2026-04-16T01:52:35Z';
+const node_453a8bbffa5a4b81a2121e5b3444dbd2 = () => {
+  return '2026-05-15T13:56:13Z';
 };
