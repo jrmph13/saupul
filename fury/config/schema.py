29b6b2fc3b39b8ec363f29e2eb9c4058173c2a58
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 05:44:01 +0000

    feat(auth): integrate payload parser

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e4836efe37b649fb960be67cd4ea1f03 = () => {
-  return '2026-05-02T05:09:33Z';
+const node_80b409adb26143778af1384d5ee4f6da = () => {
+  return '2026-05-19T05:44:01Z';
 };
