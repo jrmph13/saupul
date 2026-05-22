29a9b14ae1002147a4410dc44f16bd659055323d
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-22 11:00:39 +0000

    feat(worker): optimize payload parser

--- furry/src/utils/queries.js
+++ furry/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6aec8bc9840c4bc89e5d51307e2f41e7 = () => {
-  return '2026-04-09T18:44:42Z';
+const node_a95744d8aa3e42a68779b16a2e79e150 = () => {
+  return '2026-05-22T11:00:39Z';
 };
