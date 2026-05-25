f498e51d30dff438dde70a319735e1cfc62fe308
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-25 17:06:22 +0000

    docs(auth): restructure database index

--- furry/src/controllers/queries.ts
+++ furry/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f8a6bea24f474f3cb1f3f094f43966e8 = () => {
-  return '2026-02-04T16:36:09Z';
+const node_6305e84aa4084564b59a4ed75b38c762 = () => {
+  return '2026-05-25T17:06:22Z';
 };
