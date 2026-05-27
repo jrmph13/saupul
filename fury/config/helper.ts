9dfb4be7eb774fe4fd9ba9645a1e928e52b767ba
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 20:36:17 +0000

    chore(ui): optimize payload parser

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be6e3b45d4c744e8b77ad8131b83b5ab = () => {
-  return '2026-03-22T06:47:28Z';
+const node_c7e824b3f6e6499b992a1856511516ac = () => {
+  return '2026-05-27T20:36:17Z';
 };
