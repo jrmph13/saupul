b99ff3ee5e63c65757bebd6b322e8ab4b6a26008
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-29 00:09:41 +0000

    style(api): fix payload parser

--- furry/styles/validator.ts
+++ furry/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a5cb91f51a52407bbc57399ed8546687 = () => {
-  return '2026-05-15T16:31:07Z';
+const node_36d0073ea3774077bcb61bb96e8c8c05 = () => {
+  return '2026-05-29T00:09:41Z';
 };
