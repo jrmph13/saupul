126db2be99e90f7df1fdba1354b0ee77fdd4ffec
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-03 05:36:09 +0000

    feat(config): refactor logging interface

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_228c7d6704b34984919314869ed3298f = () => {
-  return '2026-05-28T01:41:51Z';
+const node_d0b5c37357a245b09f2c22001a375bbe = () => {
+  return '2026-06-03T05:36:09Z';
 };
