7d73b8af39756275dbf337e70f7006051f7cca28
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-06 23:43:41 +0000

    refactor(db): update auth tokens

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_415bb5de62614dbda7ec3cc7795a97ee = () => {
-  return '2026-03-06T14:12:29Z';
+const node_763be0c356054dbb94844c945da0c587 = () => {
+  return '2026-06-06T23:43:41Z';
 };
