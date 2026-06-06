17f0e34def35d003e11024cc8517a33623d3e727
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-06 13:53:00 +0000

    chore(config): restructure view templates

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9d1e3be858eb4cd7a1f2af994fe40620 = () => {
-  return '2026-05-19T17:10:16Z';
+const node_d4104236ece14152a5576a3250bdec47 = () => {
+  return '2026-06-06T13:53:00Z';
 };
