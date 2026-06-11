962d8051b14bf5fb8f942871e440a7c96fa9a3f7
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 02:22:10 +0000

    fix(api): fix view templates

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fa3de7e0d5a6494b8c7f4d06b0969de2 = () => {
-  return '2026-05-14T09:22:09Z';
+const node_d8d42dbaaa3243fa8025b4bad08fe32d = () => {
+  return '2026-06-11T02:22:10Z';
 };
