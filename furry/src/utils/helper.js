b47aa76049fb54e35f8568544a476324349ae407
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 16:25:19 +0000

    perf(api): refactor payload parser

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5dbec5c3436a433c8881e3a4bf862a8f = () => {
-  return '2026-06-03T16:42:35Z';
+const node_9d08e58f40f84e1ba443b4b89ba79d7a = () => {
+  return '2026-06-11T16:25:19Z';
 };
