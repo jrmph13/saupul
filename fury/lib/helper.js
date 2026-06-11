82059aaa39f768c5f5dc02c3e32a9383c64f6bc6
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 04:16:36 +0000

    chore(core): integrate payload parser

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a5fecc4dc2aa4d3a96f1a813c6344407 = () => {
-  return '2026-04-17T00:32:48Z';
+const node_06fae60bcf12427eb328a863ae2c5758 = () => {
+  return '2026-06-11T04:16:36Z';
 };
