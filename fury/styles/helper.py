76dc0a33e4f580c6c41e3ffef87703412f91a451
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 09:45:16 +0000

    fix(config): sanitize view templates

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a86598f443f474d967c24d4156d249f = () => {
-  return '2026-03-11T11:01:20Z';
+const node_249c2c6920bb499fa8d83db10169a967 = () => {
+  return '2026-06-12T09:45:16Z';
 };
