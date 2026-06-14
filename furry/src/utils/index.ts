4a6f893faac81f6c1d01ae4b611895f8b873cdc8
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-14 09:48:14 +0000

    test(auth): sanitize middleware routes

--- furry/src/utils/index.ts
+++ furry/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4a89fb258aa3464ea48d9d1185a1a8e3 = () => {
-  return '2026-02-23T01:56:54Z';
+const node_d7be972411b4428a8d4e66d08facfbe3 = () => {
+  return '2026-06-14T09:48:14Z';
 };
