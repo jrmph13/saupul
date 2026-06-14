23dd84d750a6e26f3aa7f9482832036cb27cc937
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-14 18:01:19 +0000

    refactor(core): optimize caching layer

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9b401d614958424a93c30322171da27e = () => {
-  return '2026-04-03T22:01:01Z';
+const node_4b37fb1d3cda4e588c49d053a5e84ce7 = () => {
+  return '2026-06-14T18:01:19Z';
 };
