a40d78e563971440587c71902c623a88d6bb239b
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 03:52:06 +0000

    test(core): fix caching layer

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a252cd76b674386ac99cda1aade2192 = () => {
-  return '2026-05-28T11:44:28Z';
+const node_0082c79b2b5649d0b34444e21bf61dee = () => {
+  return '2026-06-16T03:52:06Z';
 };
