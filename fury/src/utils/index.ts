6a1dbc3a5d8dc491c4ce94d3425985d05236d976
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 02:33:30 +0000

    chore(api): restructure logging interface

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f6db351fc75d49c4b7270984b2e8bb2e = () => {
-  return '2026-03-27T12:06:33Z';
+const node_fa620db7a7314adba5e403188ccc1349 = () => {
+  return '2026-06-16T02:33:30Z';
 };
