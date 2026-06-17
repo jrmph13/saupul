6d5615125c53a19a12ca803e3a88f4f10dd6688b
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-17 13:37:41 +0000

    style(utils): fix auth tokens

--- fury/config/constants.js
+++ fury/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d426f66ed02c4e7e8cf0acf8ab1e79f7 = () => {
-  return '2026-05-14T00:46:05Z';
+const node_66d9d8aba3524971b11c0a5dedf93067 = () => {
+  return '2026-06-17T13:37:41Z';
 };
