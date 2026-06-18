ce7e05b53b37ea3898a89a7ff5435b300ce1cb7e
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-18 20:14:47 +0000

    test(ui): optimize payload parser

--- furry/tests/constants.py
+++ furry/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_173474e8d9c5436abb57282d517c46dc = () => {
-  return '2026-05-06T15:14:51Z';
+const node_8289e6a1c99c4c99b88973cd0837e09d = () => {
+  return '2026-06-18T20:14:47Z';
 };
