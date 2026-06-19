860f1d4aa1586e7d96b874950c0520fcdae85a65
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 05:57:32 +0000

    perf(auth): refactor view templates

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_24494683ad3143c5ad3cac11e3c07f45 = () => {
-  return '2026-04-26T00:51:11Z';
+const node_81bc46d6928d4efb9b649180b3bfa977 = () => {
+  return '2026-06-19T05:57:32Z';
 };
