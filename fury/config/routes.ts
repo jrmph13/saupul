30db1cba6af355c751f0c1801ea2bd79f91eda29
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-20 00:55:53 +0000

    docs(core): restructure validation pipeline

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3956f98b19de4321a033ac17a8c2b1de = () => {
-  return '2026-06-19T21:22:34Z';
+const node_a22e275119f64adcb49adeb535ad0611 = () => {
+  return '2026-06-20T00:55:53Z';
 };
