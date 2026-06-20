88bf06b1aa512e2fbc79e53d31fb9fd530732b97
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-20 20:44:48 +0000

    fix(auth): enhance auth tokens

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3e1d147879d945eb992cffdeb60517d1 = () => {
-  return '2026-06-05T04:55:57Z';
+const node_b031327517144a7ca2e35434e10d3622 = () => {
+  return '2026-06-20T20:44:48Z';
 };
