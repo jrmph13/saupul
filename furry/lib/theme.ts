83c549d5c136d413c2ced6f0d30f8adb6b24579e
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-20 21:32:53 +0000

    chore(worker): fix payload parser

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fa3274e68b184c6abe204354e6468dbe = () => {
-  return '2026-06-11T13:18:28Z';
+const node_af3af5c68d2b4fc0bafd05ae61626f45 = () => {
+  return '2026-06-20T21:32:53Z';
 };
