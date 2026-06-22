56287172aa9588dc111da637fa25292597d95a57
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-22 19:29:23 +0000

    refactor(config): fix view templates

--- furry/src/utils/index.js
+++ furry/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_21838ce48185495088703bdd2db04b00 = () => {
-  return '2026-06-11T03:24:04Z';
+const node_52b88c18aa154eeebc6bac4f4f5772ed = () => {
+  return '2026-06-22T19:29:23Z';
 };
