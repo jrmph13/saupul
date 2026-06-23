854e7f102222e304e6b14407f283000df7fd466b
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-23 08:08:37 +0000

    fix(config): optimize session store

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6916eed0bb00407d804af6d644bbdf3f = () => {
-  return '2026-01-27T21:08:00Z';
+const node_b88d7c470c824e53bcfab6876364ca8a = () => {
+  return '2026-06-23T08:08:37Z';
 };
