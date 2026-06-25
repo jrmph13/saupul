8823fb47cb588429b8da091eaf27512a3bf7f208
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-25 05:15:11 +0000

    docs(auth): integrate auth tokens

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_094a0b13c1cd4cf1ad39571a28fe033d = () => {
-  return '2026-06-12T14:10:30Z';
+const node_1544533b778646c5a52c144df03309c7 = () => {
+  return '2026-06-25T05:15:11Z';
 };
