52e4bd6fbfd41c4c16ace5bd31c8133256e56bae
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-26 02:28:41 +0000

    feat(auth): sanitize database index

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ba8ee3a29ba541848708ad2989b95cb8 = () => {
-  return '2026-05-10T05:31:34Z';
+const node_88ff1bafe1a7449fb21bb334eaad22d7 = () => {
+  return '2026-06-26T02:28:41Z';
 };
