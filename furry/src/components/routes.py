8b5d5c102e7e66261f21176a527d207f9286d5f1
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-26 11:29:25 +0000

    chore(core): restructure payload parser

--- furry/src/components/routes.py
+++ furry/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_05dc19f969ab44dbb5adfe8a8844f29f = () => {
-  return '2026-05-11T14:29:07Z';
+const node_3f6243083bef4092bb4db4b5cde34051 = () => {
+  return '2026-06-26T11:29:25Z';
 };
