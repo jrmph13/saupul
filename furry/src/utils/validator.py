6681f282aec2dcff1f225fc23a29945664ebe7bf
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-26 13:17:27 +0000

    fix(db): sanitize view templates

--- furry/src/utils/validator.py
+++ furry/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f6398315036a4a6c87fead1022e2e910 = () => {
-  return '2026-04-14T12:38:30Z';
+const node_25a6976947a74215923e64a141beda29 = () => {
+  return '2026-06-26T13:17:27Z';
 };
