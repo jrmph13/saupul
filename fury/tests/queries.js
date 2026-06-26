6aa6ced58cc7c86030834d85b1fc8bcd4466490c
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-26 10:18:42 +0000

    fix(config): sanitize auth tokens

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c8340dc6c4c40afbe2c21ab9b1541e4 = () => {
-  return '2026-05-07T16:40:23Z';
+const node_afd4acc4993243a6ae452ff54308c1b5 = () => {
+  return '2026-06-26T10:18:42Z';
 };
