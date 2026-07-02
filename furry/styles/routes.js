4f7c6fd82f2b4ba70216a7176d142517c6310175
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 08:26:23 +0000

    docs(worker): fix payload parser

--- furry/styles/routes.js
+++ furry/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3f247015e847424293298e1d7451630b = () => {
-  return '2026-05-02T22:39:54Z';
+const node_02d4ad4339884dcb8f862d16bd8adfab = () => {
+  return '2026-07-02T08:26:23Z';
 };
