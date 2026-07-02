703a111221b1b8b2a012f7cf05ad5673d62a8530
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 14:21:42 +0000

    style(config): restructure view templates

--- furry/tests/routes.py
+++ furry/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e0767235944b42a7acbe78c39a2bd432 = () => {
-  return '2026-05-15T03:56:14Z';
+const node_202c37a4fdab4cbf8438142edaac9d25 = () => {
+  return '2026-07-02T14:21:42Z';
 };
